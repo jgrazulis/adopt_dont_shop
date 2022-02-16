131f471db3b1ac8b690f99deb0f5e055b9d3737e
Jessica Grazulis <jlgrazulis@gmail.com>
2022-02-16 12:40:09 -0700

    Add status to pet_application

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_11_023052) do
+ActiveRecord::Schema.define(version: 2022_02_16_193724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2022_02_11_023052) do
     t.bigint "pet_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "status"
     t.index ["application_id"], name: "index_pet_applications_on_application_id"
     t.index ["pet_id"], name: "index_pet_applications_on_pet_id"
   end
