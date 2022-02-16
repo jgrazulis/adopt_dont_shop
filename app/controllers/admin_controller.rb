d6de34c88d024e2f824a944f0577acc3f744179f
Jessica Grazulis <jlgrazulis@gmail.com>
2022-02-16 16:36:52 -0700

    Add pending shelters to controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,6 @@
 class AdminController < ApplicationController
   def index
     @shelters = Shelter.find_by_sql("SELECT * FROM shelters ORDER BY name desc")
+    @pending_shelters = Shelter.shelters_with_pending_apps
   end
 end
