de2928f1c82059d4db7e99e2c5accaf38ffdab18
Jessica Grazulis <jlgrazulis@gmail.com>
2022-02-16 10:43:23 -0700

    Add test for search function

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'application show page' do
   before(:each) do
     @scranton = Shelter.create!(name: "Scranton Animal Shelter", city: "Scranton, PA", foster_program: false, rank: 9)
 
-    @bob = Application.create!(name: "Bob Vance", street_address: "1234 Dunder Ave", city: "Scranton", state: "PA", zip_code: "18508" , description: "Phyllis and I love dogs, want one", status: 1)
+    @bob = Application.create!(name: "Bob Vance", street_address: "1234 Dunder Ave", city: "Scranton", state: "PA", zip_code: "18508" , description: "Phyllis and I love dogs, want one", status: 0)
     
     @mia = @bob.pets.create!(name: "Mia", age: 4, breed: "American Bulldog", adoptable: true, shelter_id: @scranton.id)
     @leo = Pet.create!(name: "Leo", age: 2, breed: "Tabby", adoptable: true, shelter_id: @scranton.id)
@@ -31,5 +31,14 @@ RSpec.describe 'application show page' do
 
   scenario 'visitor sees application status' do
     expect(page).to have_content(@bob.status)
+  end
+
+  scenario 'visitor can search pet name and will get matching results' do
+
+    fill_in('query', with: 'Mia')
+    click_button('Search')
+
+    expect(page).to have_content(@mia.name)
+    expect(page).to_not have_content(@leo.name)
   end
 end
