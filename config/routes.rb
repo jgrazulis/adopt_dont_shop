0522ea999b873300903625a35a49b3bbe0630729
Jessica Grazulis <jlgrazulis@gmail.com>
2022-02-16 17:49:35 -0700

    Fix route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get '/applications/new', to: 'applications#new'
   get '/applications/:id', to: 'applications#show'
   patch '/applications/:id', to: 'applications#update'
-  get 'applications', to: 'applications#index'
+  get '/applications', to: 'applications#index'
   post '/applications', to: 'applications#create'
 
   post 'applications/:id', to: 'pet_applications#create'
